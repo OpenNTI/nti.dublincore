001eccbb4bfb6c49c03051e7fb41f2642513991b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-05-11 16:41:58 +0000

    one more test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.dublincore/trunk@64959 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dublincore/tests/test_datastructures.py
+++ src/nti/dublincore/tests/test_datastructures.py
@@ -8,6 +8,9 @@ __docformat__ = "restructuredtext en"
 # pylint: disable=W0212,R0904
 
 from hamcrest import is_
+from hamcrest import is_not
+from hamcrest import raises
+from hamcrest import calling
 from hamcrest import assert_that
 
 import unittest
@@ -40,3 +43,48 @@ class TestPersistentExternalizableWeakList(unittest.TestCase):
         l.extend( [c1, c2, c3] )
         assert_that( l, is_( [c1, c2, c3] ) )
         assert_that( l, is_(l) )
+        
+    def test_mutate(self):
+
+        obj = PersistentExternalizableWeakList()
+
+        # Cannot set non-persistent objects
+        assert_that( calling( obj.append ).with_args(object()),
+                     raises(AttributeError) )
+
+        pers = persistent.Persistent()
+        obj.append( pers )
+        assert_that( obj[0], is_( pers ) )
+
+        pers2 = persistent.Persistent()
+        obj[0] = pers2
+        assert_that( obj[0], is_( pers2 ) )
+        assert_that( obj.count( pers2 ), is_( 1 ) )
+        assert_that( obj.count( pers ), is_( 0 ) )
+
+        # iteration
+        for x in obj:
+            assert_that( x, is_( pers2 ) )
+        assert_that( obj.index( pers2 ), is_( 0 ) )
+
+        assert_that( obj.pop(), is_( pers2 ) )
+        assert_that( calling(obj.pop), raises(IndexError) )
+
+        assert_that( obj, is_( obj ) )
+
+        obj.append( pers2 )
+        # mul
+        assert_that( obj * 2, is_( PersistentExternalizableWeakList( [pers2, pers2] ) ) )
+
+        # imul
+        obj *= 2
+        assert_that( obj, is_( PersistentExternalizableWeakList( [pers2, pers2] ) ) )
+
+        obj.pop()
+        # insert
+        obj.insert( 1, pers2 )
+        assert_that( obj, is_( PersistentExternalizableWeakList( [pers2, pers2] ) ) )
+
+        assert_that( obj, is_( [pers2, pers2] ) )
+        assert_that( obj, is_not( [pers2, pers] ) )
+        assert_that( obj, is_not( pers ) )
