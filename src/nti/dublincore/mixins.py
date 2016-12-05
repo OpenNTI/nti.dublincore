52561368626bbc5ffb1a95b6a7a6ad4ec417819d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-12-05 00:29:40 +0000

    add private module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.dublincore/trunk@101925 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dublincore/mixins.py
+++ src/nti/dublincore/mixins.py
@@ -14,40 +14,10 @@ try:
     from nti.coremetadata.mixins import ModifiedTimeMixin
     from nti.coremetadata.mixins import CreatedAndModifiedTimeMixin
 except ImportError:
-    import time
-    
-    class CreatedTimeMixin(object):
+    from nti.dublincore._mixins import CreatedTimeMixin
+    from nti.dublincore._mixins import ModifiedTimeMixin
+    from nti.dublincore._mixins import CreatedAndModifiedTimeMixin
 
-        _SET_CREATED_MODTIME_ON_INIT = True
-    
-        createdTime = 0
-    
-        def __init__(self, *args, **kwargs):
-            if self._SET_CREATED_MODTIME_ON_INIT and self.createdTime == 0:
-                self.createdTime = time.time()
-            super(CreatedTimeMixin, self).__init__(*args, **kwargs)
-    
-    class ModifiedTimeMixin(object):
-
-        lastModified = 0
-    
-        def __init__(self, *args, **kwargs):
-            super(ModifiedTimeMixin, self).__init__(*args, **kwargs)
-    
-        def updateLastMod(self, t=None):
-            now = time.time()
-            self.lastModified = (t if t is not None and t > self.lastModified else now)
-            return self.lastModified
-    
-        def updateLastModIfGreater(self, t):
-            if t is not None and t > self.lastModified:
-                self.lastModified = t
-            return self.lastModified
-        
-    class CreatedAndModifiedTimeMixin(CreatedTimeMixin, ModifiedTimeMixin):
-    
-        def __init__(self, *args, **kwargs):
-            if self._SET_CREATED_MODTIME_ON_INIT:
-                self.createdTime = time.time()
-                self.updateLastModIfGreater(self.createdTime)
-            super(CreatedAndModifiedTimeMixin, self).__init__(*args, **kwargs)
+CreatedTimeMixin = CreatedTimeMixin
+ModifiedTimeMixin = ModifiedTimeMixin
+CreatedAndModifiedTimeMixin = CreatedAndModifiedTimeMixin
