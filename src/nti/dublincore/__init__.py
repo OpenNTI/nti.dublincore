373824a51ea709bbf7535fe5e7ec6a7f7ee696c6
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-05-09 22:32:01 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.dublincore/trunk@112389 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/dublincore/__init__.py
+++ src/nti/dublincore/__init__.py
@@ -7,16 +7,7 @@ and building on :mod:`zope.dublincore`.
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
-
-from nti.dublincore.datastructures import CreatedModDateTrackingObject
-from nti.dublincore.datastructures import PersistentCreatedModDateTrackingObject
-
-from nti.dublincore.time_mixins import TimeProperty
-from nti.dublincore.time_mixins import ModifiedTimeMixin
-
-from nti.dublincore.time_mixins import CreatedAndModifiedTimeMixin
-from nti.dublincore.time_mixins import PersistentCreatedAndModifiedTimeObject
